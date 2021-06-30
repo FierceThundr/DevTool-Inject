05e07d324154682a29a03c2e59dacbaeb7692413
FierceThundr <funman4356@gmail.com>
2021-06-30 13:07:42 -0400

    Various changes
    Removed Function
    Uploaded minimized css
    Added CSS injection

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-function transform() {
-	//document.getElementsByTagName('head')[0].appendChild(document.createElement('script')).src='http://domain.com/scripts/external.js?'
+document.getElementsByTagName('head')[0].appendChild(document.createElement('link')).href='https://fiercethundr.github.io/DevTool-Inject/style.css'
 	
-	var element = document.createElement("div")
-	element.setAttribute("id","new_ui");
-	document.body.insertBefore(element,document.body.firstChild);
+var element = document.createElement("div")
+element.setAttribute("id","new_ui");
+document.body.insertBefore(element,document.body.firstChild);
 	
-	
-	
-	document.getElementById("new_ui").style.top = "0";
-}
-
-console.log('Injection success')
-
-setTimeout(transform,5000)
+document.getElementById("new_ui").style.top = "0";
