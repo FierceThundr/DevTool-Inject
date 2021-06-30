2911a3a13caad82e570daa5c4a16ae8b22fa54f0
FierceThundr <funman4356@gmail.com>
2021-06-30 12:48:33 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function transform() {
+	//document.getElementsByTagName('head')[0].appendChild(document.createElement('script')).src='http://domain.com/scripts/external.js?'
+	
+	var element = document.createElement("div")
+	element.setAttribute("id","new_ui");
+	document.body.insertBefore(element,document.body.firstChild);
+	
+	
+	
+	document.getElementById("new_ui").style.top = "0";
+}
